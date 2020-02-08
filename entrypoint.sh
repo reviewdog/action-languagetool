1cf5cba965d2289a587d3bf1d4f6ea6259c27b18
haya14busa <haya14busa@gmail.com>
2020-02-08 08:01:55 +0000

    fix data arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,23 +8,35 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+# https://languagetool.org/http-api/swagger-ui/#!/default/post_check
+DATA="language=${INPUT_LANGUAGE}"
+if [ -n "${INPUT_ENABLED_RULES}" ]; then
+  DATA="$DATA&enabledRules=${INPUT_ENABLED_RULES}"
+fi
+if [ -n "${INPUT_DISABLED_RULES}" ]; then
+  DATA="$DATA&disabledRules=${INPUT_DISABLED_RULES}"
+fi
+if [ -n "${INPUT_ENABLED_CATEGORIES}" ]; then
+  DATA="$DATA&enabledCategories=${INPUT_ENABLED_CATEGORIES}"
+fi
+if [ -n "${INPUT_DISABLED_CATEGORIES}" ]; then
+  DATA="$DATA&disabledCategories=${INPUT_DISABLED_CATEGORIES}"
+fi
+if [ -n "${INPUT_ENABLED_ONLY}" ]; then
+  DATA="$DATA&enabledOnly=${INPUT_ENABLED_ONLY}"
+fi
 
 run_langtool() {
   for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
-    # https://languagetool.org/http-api/swagger-ui/#!/default/post_check
     curl \
-      --data-urlencode "language=${INPUT_LANGUAGE}" \
-      --data-urlencode "enabledRules=${INPUT_ENABLED_RULES}" \
-      --data-urlencode "disabledRules=${INPUT_DISABLED_RULES}" \
-      --data-urlencode "enabledCategories=${INPUT_ENABLED_CATEGORIES}" \
-      --data-urlencode "disabledCategories=${INPUT_DISABLED_CATEGORIES}" \
-      --data-urlencode "enabledOnly=${INPUT_ENABLED_ONLY}" \
+      --data "${DATA}" \
       --data-urlencode "text=$(cat "${FILE}")" \
       http://localhost:8010/v2/check | \
       FILE="${FILE}" tmpl /langtool.tmpl
   done
 }
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 run_langtool \
   | reviewdog -efm="%A%f:%l:%c: %m" -efm="%C %m" -name="LanguageTool" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
