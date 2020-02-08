c95ca7469cac5a55db4a0fad6fc6bafa97680ae0
haya14busa <haya14busa@gmail.com>
2020-02-08 07:08:17 +0000

    Use reviewdog to report results

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ set -e
 LANGTOOL_DATA="language=en-US"
 
 java -cp "/LanguageTool-${LANGUAGETOOL_VERSION}/languagetool-server.jar" org.languagetool.server.HTTPServer --port 8010 &
-sleep 3 # Wait the server statup.
-curl --data "language=en-US&text=a simple test" http://localhost:8010/v2/check
+sleep 1 # Wait the server statup.
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
@@ -13,13 +12,14 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
-  echo "FILE: $FILE"
-  curl --data "language=en-US" \
-    --data-urlencode "text=$(cat "${FILE}")" \
-    http://localhost:8010/v2/check | \
-    FILE="${FILE}" tmpl /langtool.tmpl
-done
+run_langtool() {
+  for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
+    curl --data "language=en-US" \
+      --data-urlencode "text=$(cat "${FILE}")" \
+      http://localhost:8010/v2/check | \
+      FILE="${FILE}" tmpl /langtool.tmpl
+  done
+}
 
-# misspell -locale="${INPUT_LOCALE}" . \
-#   | reviewdog -efm="%f:%l:%c: %m" -name="linter-name (misspell)" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+run_langtool \
+  | reviewdog -efm="%A%f:%l:%c: %m" -efm="%C %m" -name="LanguageTool" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
