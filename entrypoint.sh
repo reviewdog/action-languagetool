756765376a9c4da9492ff3de3999d21a6c344670
haya14busa <haya14busa@gmail.com>
2020-02-08 07:47:39 +0000

    Use --data

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,19 +20,14 @@ run_langtool() {
   echo "disabledCategories=${INPUT_DISABLED_CATEGORIES}" >&2
   echo "enabledOnly=${INPUT_ENABLED_ONLY}" >&2
 
-  # for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
-  #   # https://languagetool.org/http-api/swagger-ui/#!/default/post_check
-  #   curl \
-  #     --data-urlencode "langugage=${INPUT_LANGUAGE}" \
-  #     --data-urlencode "enabledRules=${INPUT_ENABLED_RULES}" \
-  #     --data-urlencode "disabledRules=${INPUT_DISABLED_RULES}" \
-  #     --data-urlencode "enabledCategories=${INPUT_ENABLED_CATEGORIES}" \
-  #     --data-urlencode "disabledCategories=${INPUT_DISABLED_CATEGORIES}" \
-  #     --data-urlencode "enabledOnly=${INPUT_ENABLED_ONLY}" \
-  #     --data-urlencode "text=$(cat "${FILE}")" \
-  #     http://localhost:8010/v2/check | \
-  #     FILE="${FILE}" tmpl /langtool.tmpl
-  # done
+  for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
+    # https://languagetool.org/http-api/swagger-ui/#!/default/post_check
+    curl \
+      --data "langugage=${INPUT_LANGUAGE}&enabledRules=${INPUT_ENABLED_RULES}&disabledRules=${INPUT_DISABLED_RULES}&enabledCategories=${INPUT_ENABLED_CATEGORIES}&disabledCategories=${INPUT_DISABLED_CATEGORIES}&enabledOnly=${INPUT_ENABLED_ONLY}"
+      --data-urlencode "text=$(cat "${FILE}")" \
+      http://localhost:8010/v2/check | \
+      FILE="${FILE}" tmpl /langtool.tmpl
+  done
 }
 
 run_langtool \
