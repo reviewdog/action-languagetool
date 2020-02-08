583cd10a932a67c2374ed6e1350478d590fdb0bd
haya14busa <haya14busa@gmail.com>
2020-02-08 07:42:29 +0000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -e
 
-# test
-echo "langugage=${INPUT_LANGUAGE}"
-echo "enabledRules=${INPUT_ENABLED_RULES}"
-echo "disabledRules=${INPUT_DISABLED_RULES}"
-echo "enabledCategories=${INPUT_ENABLED_CATEGORIES}"
-echo "disabledCategories=${INPUT_DISABLED_CATEGORIES}"
-echo "enabledOnly=${INPUT_ENABLED_ONLY}"
-exit
 
 java -cp "/LanguageTool-${LANGUAGETOOL_VERSION}/languagetool-server.jar" org.languagetool.server.HTTPServer --port 8010 &
 sleep 3 # Wait the server statup.
@@ -20,19 +12,27 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 run_langtool() {
-  for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
-    # https://languagetool.org/http-api/swagger-ui/#!/default/post_check
-    curl \
-      --data-urlencode "langugage=${INPUT_LANGUAGE}" \
-      --data-urlencode "enabledRules=${INPUT_ENABLED_RULES}" \
-      --data-urlencode "disabledRules=${INPUT_DISABLED_RULES}" \
-      --data-urlencode "enabledCategories=${INPUT_ENABLED_CATEGORIES}" \
-      --data-urlencode "disabledCategories=${INPUT_DISABLED_CATEGORIES}" \
-      --data-urlencode "enabledOnly=${INPUT_ENABLED_ONLY}" \
-      --data-urlencode "text=$(cat "${FILE}")" \
-      http://localhost:8010/v2/check | \
-      FILE="${FILE}" tmpl /langtool.tmpl
-  done
+  # test
+  echo "langugage=${INPUT_LANGUAGE}"
+  echo "enabledRules=${INPUT_ENABLED_RULES}"
+  echo "disabledRules=${INPUT_DISABLED_RULES}"
+  echo "enabledCategories=${INPUT_ENABLED_CATEGORIES}"
+  echo "disabledCategories=${INPUT_DISABLED_CATEGORIES}"
+  echo "enabledOnly=${INPUT_ENABLED_ONLY}"
+
+  # for FILE in $(git ls-files | ghglob '**/*.md' '**/*.txt'); do
+  #   # https://languagetool.org/http-api/swagger-ui/#!/default/post_check
+  #   curl \
+  #     --data-urlencode "langugage=${INPUT_LANGUAGE}" \
+  #     --data-urlencode "enabledRules=${INPUT_ENABLED_RULES}" \
+  #     --data-urlencode "disabledRules=${INPUT_DISABLED_RULES}" \
+  #     --data-urlencode "enabledCategories=${INPUT_ENABLED_CATEGORIES}" \
+  #     --data-urlencode "disabledCategories=${INPUT_DISABLED_CATEGORIES}" \
+  #     --data-urlencode "enabledOnly=${INPUT_ENABLED_ONLY}" \
+  #     --data-urlencode "text=$(cat "${FILE}")" \
+  #     http://localhost:8010/v2/check | \
+  #     FILE="${FILE}" tmpl /langtool.tmpl
+  # done
 }
 
 run_langtool \
