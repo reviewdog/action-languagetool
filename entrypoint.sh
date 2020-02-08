adf10affc1265a6238cbf834390bdb87a1b651e9
haya14busa <haya14busa@gmail.com>
2020-02-08 06:16:17 +0000

    fix langaguetool server command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
+# shellcheck disable=SC1091
+source start.sh &
+echo 'LanguageTool server started?'
+
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-source start.sh &
-echo 'LanguageTool server started?'
 
 # misspell -locale="${INPUT_LOCALE}" . \
 #   | reviewdog -efm="%f:%l:%c: %m" -name="linter-name (misspell)" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
