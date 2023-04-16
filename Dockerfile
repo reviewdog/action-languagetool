2e0602e07f9f56f574aa7e5c77b1cb4d697818e4
Harjot Gill <harjot.gill@fluxninja.com>
2023-04-15 21:34:40 -0700

    update (#3)

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM erikvl87/languagetool:5.9
+FROM erikvl87/languagetool:6.1
 # https://github.com/Erikvl87/docker-languagetool
 
 ENV REVIEWDOG_VERSION=v0.14.1
 ENV TMPL_VERSION=v1.2.0
 ENV OFFSET_VERSION=v1.0.6
-ENV LANGUAGETOOL_VERSION=5.2
+ENV LANGUAGETOOL_VERSION=6.1
 ENV GHGLOB_VERSION=v2.0.2
 
 USER root
@@ -12,7 +12,7 @@ USER root
 SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
 
 # hadolint ignore=DL3006
-RUN apk --no-cache add git curl
+RUN apk --no-cache add git curl jq
 
 RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION} && \
   wget -O - -q https://raw.githubusercontent.com/haya14busa/tmpl/master/install.sh| sh -s -- -b /usr/local/bin/ ${TMPL_VERSION} && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,55 +3,75 @@ set -eo pipefail
 
 API_ENDPOINT="${INPUT_CUSTOM_API_ENDPOINT}"
 if [ -z "${INPUT_CUSTOM_API_ENDPOINT}" ]; then
-  API_ENDPOINT=http://localhost:8010
-  java -cp "/LanguageTool/languagetool-server.jar" org.languagetool.server.HTTPServer --port 8010 &
-  sleep 3 # Wait the server statup.
+	API_ENDPOINT=http://localhost:8010
+	java -cp "/LanguageTool/languagetool-server.jar" org.languagetool.server.HTTPServer --port 8010 &
+	sleep 3 # Wait the server statup.
 fi
 
 echo "API ENDPOINT: ${API_ENDPOINT}" >&2
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
-  cd "${GITHUB_WORKSPACE}" || exit
+	cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-git config --global --add safe.directory $GITHUB_WORKSPACE
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
 # https://languagetool.org/http-api/swagger-ui/#!/default/post_check
 DATA="language=${INPUT_LANGUAGE}"
 if [ -n "${INPUT_ENABLED_RULES}" ]; then
-  DATA="$DATA&enabledRules=${INPUT_ENABLED_RULES}"
+	DATA="$DATA&enabledRules=${INPUT_ENABLED_RULES}"
 fi
 if [ -n "${INPUT_DISABLED_RULES}" ]; then
-  DATA="$DATA&disabledRules=${INPUT_DISABLED_RULES}"
+	DATA="$DATA&disabledRules=${INPUT_DISABLED_RULES}"
 fi
 if [ -n "${INPUT_ENABLED_CATEGORIES}" ]; then
-  DATA="$DATA&enabledCategories=${INPUT_ENABLED_CATEGORIES}"
+	DATA="$DATA&enabledCategories=${INPUT_ENABLED_CATEGORIES}"
 fi
 if [ -n "${INPUT_DISABLED_CATEGORIES}" ]; then
-  DATA="$DATA&disabledCategories=${INPUT_DISABLED_CATEGORIES}"
+	DATA="$DATA&disabledCategories=${INPUT_DISABLED_CATEGORIES}"
 fi
 if [ -n "${INPUT_ENABLED_ONLY}" ]; then
-  DATA="$DATA&enabledOnly=${INPUT_ENABLED_ONLY}"
+	DATA="$DATA&enabledOnly=${INPUT_ENABLED_ONLY}"
+fi
+if [ -n "${INPUT_USERNAME}" ]; then
+	DATA="$DATA&username=${INPUT_USERNAME}"
+fi
+if [ -n "${INPUT_API_KEY}" ]; then
+	DATA="$DATA&apiKey=${INPUT_API_KEY}"
 fi
 
 # Disable glob to handle glob patterns with ghglob command instead of with shell.
 set -o noglob
-FILES="$(git ls-files | ghglob ${INPUT_PATTERNS})"
+
+if [ "${INPUT_FILTER_MODE}" = "changed" ]; then
+	PR_NUMBER=$(echo "${GITHUB_REF}" | awk -F / '{print $3}')
+	FILES="$(curl --silent -H "Authorization: token ${INPUT_GITHUB_TOKEN}" \
+		"https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/files" |
+		jq -r '.[] | select(.status != "deleted") | .filename' |
+		ghglob "${INPUT_PATTERNS}")"
+else
+	FILES="$(git ls-files | ghglob "${INPUT_PATTERNS}")"
+fi
+
+# echo list of files to check
+echo "Files to check:"
+echo "${FILES}"
+
 set +o noglob
 
 run_langtool() {
-  for FILE in ${FILES}; do
-    echo "Checking ${FILE}..." >&2
-    curl --silent \
-      --request POST \
-      --data "${DATA}" \
-      --data-urlencode "text=$(cat "${FILE}")" \
-      "${API_ENDPOINT}/v2/check" | \
-      FILE="${FILE}" tmpl /langtool.tmpl
-  done
+	for FILE in ${FILES}; do
+		echo "Checking ${FILE}..." >&2
+		curl --silent \
+			--request POST \
+			--data "${DATA}" \
+			--data-urlencode "text=$(cat "${FILE}")" \
+			"${API_ENDPOINT}/v2/check" |
+			FILE="${FILE}" tmpl /langtool.tmpl
+	done
 }
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-run_langtool \
-  | reviewdog -efm="%A%f:%l:%c: %m" -efm="%C %m" -name="LanguageTool" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+run_langtool |
+	reviewdog -efm="%A%f:%l:%c: %m" -efm="%C %m" -name="LanguageTool" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
